37f97629203e06f72bb761ec0c37fa85917b6412
Amir Makhmudov <amir.makhmudov.f@gmail.com>
2025-10-18 21:22:40 +0300

    Выполнено два задания.

---
+++ src/ExpensesManager.java
@@ -0,0 +1,65 @@
+import java.util.ArrayList;
+
+public class ExpensesManager {
+    ArrayList<Expense> expenses;
+
+    ExpensesManager() {
+        expenses = new ArrayList<>();
+    }
+
+    double saveExpense(double moneyBeforeSalary, double expense) {
+        moneyBeforeSalary = moneyBeforeSalary - expense;
+        expenses.add(new Expense(expense));
+        System.out.println("Значение сохранено! Ваш текущий баланс в рублях: " + moneyBeforeSalary);
+        if (moneyBeforeSalary < 1000) {
+            System.out.println("На вашем счету осталось совсем немного. Стоит начать экономить!");
+        }
+        return moneyBeforeSalary;
+    }
+
+    void printAllExpenses() {
+        for (int i = 0; i < expenses.size(); i++) {
+            Expense exp = expenses.get(i);
+            System.out.println("Трата № " + (i + 1) + ". Потрачено " + exp.getValue() + " рублей, код транзакции: "+exp.getTransaction());
+        }
+    }
+
+    double findMaxExpense() {
+        double maxExpense = 0;
+        for (Expense exp : expenses) {
+            if (exp.getValue() > maxExpense) {
+                maxExpense = exp.getValue();
+            }
+        }
+        return maxExpense;
+    }
+
+    // Добавьте метод removeAllExpenses()
+    public void removeAllExpenses() {
+        expenses.clear();
+        System.out.println("Список трат пуст.");
+    }
+
+    // Добавьте метод removeExpense(int transaction)
+    public void removeExpense(int transaction) {
+        if (expenses.isEmpty()) {
+            System.out.println("Список трат пуст.");
+            return;
+        }
+
+        int index = -1;
+        for (int i = 0; i < expenses.size(); i++) {
+            if (expenses.get(i).getTransaction() == transaction) {
+                index = i;
+                break;
+            }
+        }
+
+        if (index != -1) {
+            expenses.remove(index);
+            System.out.println("Трата удалена!");
+        } else {
+            System.out.println("Такой траты нет.");
+        }
+    }
+}
